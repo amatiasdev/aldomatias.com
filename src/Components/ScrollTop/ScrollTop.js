c0729708a8c5ad9c32b864805a1f65547253b05c
ARMO <aldomatias@protonmail.com>
2021-02-01 19:33:23 -0600

    Se elimina proptypes incesesarios de ScrollTop

--- src/Components/ScrollTop/ScrollTop.js
+++ src/Components/ScrollTop/ScrollTop.js
@@ -1,8 +1,7 @@
 
 import Zoom from '@material-ui/core/Zoom';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import { makeStyles } from '@material-ui/core/styles';
-import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles'; 
 import GoToID from '../../Helpers/GoToID';
 
 const useStyles = makeStyles((theme) => ({
@@ -17,9 +16,6 @@ const ScrollTop = props => {
     
     const { children, window } = props;
     const classes = useStyles();
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
     const trigger = useScrollTrigger({
       target: window ? window() : undefined,
       disableHysteresis: true,
@@ -34,14 +30,5 @@ const ScrollTop = props => {
       </Zoom>
     );
   }
-  
-  ScrollTop.propTypes = {
-    children: PropTypes.element.isRequired,
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
-    window: PropTypes.func,
-  };
 
   export default ScrollTop;
